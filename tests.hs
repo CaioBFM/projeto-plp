08651f56db835c7b0d31c28db9891bc1e81bc978
CaioBFM <caiofinocchio@outlook.com>
2025-06-03 18:36:35 -0300

    test: testes

--- tests.hs
+++ tests.hs
@@ -2,5 +2,17 @@
 
 menorDeDois :: (Ord t) => t -> t -> t
 menorDeDois a b
-	| a < b     = a
-	| otherwise = b
+    | a < b     = a
+    | otherwise = b
+
+-- questao 1
+ultimo :: [t] -> t
+ultimo [u] = u
+ultimo (c:r) = ultimo r
+
+-- questao 4
+maiores_que :: (Ord t) => t -> [t] -> [t]
+maiores_que v (c:r)
+    | v < c     = c : maiores_que v r
+    | otherwise = maiores_que v r
+maiores_que _ [] = []
